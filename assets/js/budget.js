154c74ed2ddfc5b387bb79a6867bd98342b1e46f
Andrew <luzkovskyandrey@gmail.com>
2014-07-06 23:17:54 +0300

    budget

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -3,16 +3,14 @@ var React = require('react'),
  Tabs = require('./componets/Tabs');
 
 var Panel = React.createClass({
-    getInitialState: function() {
-        return { colapsed: true };
-      },
-    toggle : function(){
-        var state = this.state.colapsed;
-        this.setState({ colapsed : !state })
+    toggle : function(e){
+        var title = e.target.dataset.title;
+        this.props.onClick(this.props.title, !this.props.colapsed);
     },
     render: function(){
-        var itemClass = "item " + (this.state.colapsed ? " " : " open");
-        var btnClass = "active fa" +(this.state.colapsed ? " fa-angle-down" : " fa-angle-up");
+        var colapsed = this.props.colapsed ;
+        var itemClass = "item " + (colapsed ? " " : " open");
+        var btnClass = "active fa" +(colapsed ? " fa-angle-down" : " fa-angle-up");
 
         var tabsBlock;
         if (this.props.title == 'Expenditure'){
@@ -24,7 +22,7 @@ var Panel = React.createClass({
         }
         return <div  className={itemClass}>
                 <div className="offset">
-                    <div onClick={this.toggle}  className="title">
+                    <div onClick={this.toggle} data-title={this.props.title}  className="title">
                         <h2 >{this.props.title}</h2>
                         <a className={btnClass}>
                         </a>
@@ -54,36 +52,53 @@ var Panel = React.createClass({
 })
 
 module.exports = React.createClass({
-    
+    handleClick : function(title, open){
+        var categories = this.state.categories;
+        categories.forEach(function(c){
+            c.open = false;
+            if (c.title == title){
+                c.open = !c.open;
+            }
+        })
+        this.setState({ categories : categories})
+    },
+    getInitialState: function() {
+        return { 
+            selected : 'Income',
+            categories: [
+                { title : 'Income', open : true},
+                { title :'Expenditure', open : false},
+                { title :'Savings', open : false}
+            ] 
+        }
+    },
     render: function () {
-        var items = [{ 
-            status : 'none', 
-            title : 'Take Home Pay', 
-            planing : '$57', 
-            actual : '$66,999' 
-        },
-        { 
-            status : 'completed', 
-            title : 'Take Home Pay', 
-            planing : '$57', 
-            actual : '$66,999' 
-        },
-        { 
-            status : 'skip', 
-            title : 'Take Home Pay', 
-            planing : '$57', 
-            actual : '$66,999' 
-        }];
-        var categories = [
-            { title : 'Income', open : true},
-            { title :'Expenditure', open : false},
-            { title :'Savings', open : false}
+        var items = [
+            { 
+                status : 'none', 
+                title : 'Take Home Pay', 
+                planing : '$57', 
+                actual : '$66,999' 
+            },
+            { 
+                status : 'completed', 
+                title : 'Take Home Pay', 
+                planing : '$57', 
+                actual : '$66,999' 
+            },
+            { 
+                status : 'skip', 
+                title : 'Take Home Pay', 
+                planing : '$57', 
+                actual : '$66,999' 
+            }
         ];
+        
         return  <div className="budget">
-                    <div onClick={this.toggle} className="budget-container">
-                    {categories.map(function(c){
-                        return <Panel  title={c.title} items={items} />
-                    })}
+                    <div className="budget-container">
+                    {this.state.categories.map(function(c){
+                        return <Panel selected={this.state.selected} onClick={this.handleClick} colapsed={!c.open} title={c.title} items={items} />
+                    }.bind(this))}
                     </div>
     			</div>
     }
